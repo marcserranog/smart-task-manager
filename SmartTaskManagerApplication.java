2420c05fcd0d923f4e4a244bd7714c4d0771c0f9
marcserranog <91466710+marcserranog@users.noreply.github.com>
2025-01-31 16:34:13 +0100

    Add files via upload
    First commit

---
+++ SmartTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.smarttaskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTaskManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartTaskManagerApplication.class, args);
+    }
+
+}
---
+++ test/java/com/example/smarttaskmanager/SmartTaskManangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smarttaskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTaskManangerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
